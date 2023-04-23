b58ed6ea77fd1841a0e678943dea312b2c56fda6
yanicebelhadj <yanice78955@gmail.com>
2023-04-23 23:46:20 +0200

    dernère modif

---
+++ src/context/userContext.js
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect } from "react";
+
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged
+} from "firebase/auth"
+
+import { auth } from "../firebase-config"
+
+export const UserContext = createContext()
+
+export function UserContextProvider(props) {
+  const signUp = (email, pwd) => createUserWithEmailAndPassword(auth, email, pwd)
+  const signIn = (email, pwd) => signInWithEmailAndPassword(auth, email, pwd)
+
+  const [currentUser, setCurrentUser] = useState();
+  const [loadingData, setLoadingData] = useState(true);
+  console.log("MAJ", currentUser);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setCurrentUser(currentUser)
+      setLoadingData(false)
+    })
+    return unsubscribe;
+  }, [])
+
+
+    return (
+        <UserContext.Provider value={{signUp, signIn, currentUser}}>
+            {!loadingData && props.children}
+        </UserContext.Provider>
+    );
+}
---
+++ src/firebase-config.js
@@ -0,0 +1,47 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    console.log(userAuth)
+
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};

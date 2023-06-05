b5662a4016dbcb714d29c9cf8116b5f796a81d26
yanicebelhadj <yanice78955@gmail.com>
2023-06-05 16:50:22 +0200

    searchpage responsive done

--- src/routes/SearchPage/SearchPage.jsx
+++ src/routes/SearchPage/SearchPage.jsx
@@ -1,17 +1,17 @@
 import ChevronBottom from "../../icons/ChevronBottom";
 
 import "./SearchPage.css";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux"; REMETTRE ÇA POUR LE MENU
 
 import dataChapitres from '../../content/content.json'
 
 import Chapitre from "../../components/competence/competence";
 
 function SearchPage() {
-  const menu = useSelector(state=> state.menu.open)
+  // const menu = useSelector(state=> state.menu.open) REMETTRE ÇA POUR LE MENU
 
   return (
-    <div className={`Search-page ${menu ? "container-initialState" : "container-clickedState"}`}>
+     <div className={`Search-page container`}>  {/* REMETTRE ÇA POUR LE MENU ${menu ? "container-initialState" : "container-clickedState"}*/} 
       <div className="filters">
         <div className="filter">
           <p className="p-m-bold">Niveau</p>

80e68a13c2423338109ab3483192a51f2a72b738
yanicebelhadj <yanice78955@gmail.com>
2023-06-12 00:44:49 +0200

    WIP

--- src/routes/SearchPage/SearchPage.jsx
+++ src/routes/SearchPage/SearchPage.jsx
@@ -1,4 +1,4 @@
-import ChevronBottom from "../../icons/ChevronBottom";
+import React, { useState } from "react";
 
 import "./SearchPage.css";
 // import { useSelector } from "react-redux"; REMETTRE ÇA POUR LE MENU
@@ -7,44 +7,81 @@ import dataChapitres from '../../content/content.json'
 
 import Chapitre from "../../components/competence/competence";
 
+
 function SearchPage() {
+  const [selectedNiveauValue, setSelectedNiveauValue] = useState('');
+  const [selectedThemeValue, setSelectedThemeValue] = useState('');
+
+  const handleSelectNiveauChange = (event) => {
+    setSelectedNiveauValue(event.target.value);
+  };
+
+  const handleSelectThemeChange = (event) => {
+    setSelectedThemeValue(event.target.value);
+  };
+  
   // const menu = useSelector(state=> state.menu.open) REMETTRE ÇA POUR LE MENU
+
+  let dataChapitresFiltered = dataChapitres;
+
+  if(selectedNiveauValue){
+    dataChapitresFiltered = dataChapitres.filter(chapitre => chapitre.niveau === parseInt(selectedNiveauValue,10))
+  }
+
+  if(selectedThemeValue){
+    dataChapitresFiltered = dataChapitres.filter(chapitre => chapitre.theme === parseInt(selectedThemeValue,10))
+  }
+
+  if(selectedNiveauValue && selectedThemeValue){
+    dataChapitresFiltered = dataChapitres.filter(chapitre => chapitre.theme === parseInt(selectedThemeValue,10) && chapitre.niveau === parseInt(selectedNiveauValue,10))
+  }
 
   return (
      <div className={`Search-page container`}>  {/* REMETTRE ÇA POUR LE MENU ${menu ? "container-initialState" : "container-clickedState"}*/} 
       <div className="filters">
-        <div className="filter">
-          <p className="p-m-bold">Niveau</p>
-          <ChevronBottom />
-        </div>
-        <div className="filter">
-          <p className="p-m-bold">Catégorie</p>
-          <ChevronBottom />
-        </div>
+        <select className="filter" value={selectedNiveauValue} onChange={handleSelectNiveauChange}>
+          <option value="">Niveau</option>
+          <option value="6">6ème</option>
+          <option value="5">5ème</option>
+          <option value="4">4ème</option>
+          <option value="3">3ème</option>
+        </select>
+
+        <select className="filter" value={selectedThemeValue} onChange={handleSelectThemeChange}>
+          <option value="">Thème</option>
+          <option value="1">Nombre et calcul</option>
+          <option value="2">Espace et géométrie</option>
+          <option value="3">Organisation et gestion de données</option>
+          <option value="4">Grandeurs et mesures</option>
+        </select>
       </div>
 
       <div className="competence-list">
         {
-          dataChapitres.map((competence, index)=>{ 
-              const { id, nomChapitre, nomTheme, description, niveau, duree, iconChapitre, slug } = competence;
-              return(
+          dataChapitresFiltered.map((competence, index)=>{ 
+              const { id, nomChapitre, theme,  nomTheme, description, niveau, duree, iconChapitre, slug } = competence;
+
+              return(              
                 <Chapitre
                   key={index}
                   slug={slug}
                   id={id}
+                  theme={theme}
                   iconChapitre={iconChapitre}
                   competenceName={nomChapitre}
                   duree={duree} 
                   nomTheme={nomTheme}
                   description = {description}
                   niveau={niveau}
-                  />
+                />
               )
           })
         }
       </div>
     </div>
   );
+  
+  
 }
 
 export default SearchPage;
